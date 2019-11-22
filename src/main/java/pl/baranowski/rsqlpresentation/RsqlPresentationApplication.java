659e6f8bf0fa94e82e6b8aed7b7a1fdd38ec756e
Marcin Baranowski <m.baranowskimarcin@gmail.com>
2019-11-22 10:16:23 +0100

    Init commit

---
+++ src/main/java/pl/baranowski/rsqlpresentation/RsqlPresentationApplication.java
@@ -0,0 +1,13 @@
+package pl.baranowski.rsqlpresentation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RsqlPresentationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RsqlPresentationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/baranowski/rsqlpresentation/RsqlPresentationApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.baranowski.rsqlpresentation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RsqlPresentationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
