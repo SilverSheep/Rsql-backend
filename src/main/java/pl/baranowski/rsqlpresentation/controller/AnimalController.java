2587c01d4ee21460ad836398609850ffc41baea0
Marcin Baranowski <m.baranowskimarcin@gmail.com>
2019-11-30 21:20:24 +0100

    Adds pagination and sorting

--- src/main/java/pl/baranowski/rsqlpresentation/controller/AnimalController.java
+++ src/main/java/pl/baranowski/rsqlpresentation/controller/AnimalController.java
@@ -1,13 +1,14 @@
 package pl.baranowski.rsqlpresentation.controller;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.domain.Specification;
 import org.springframework.web.bind.annotation.*;
 import pl.baranowski.rsqlpresentation.model.Animal;
 import pl.baranowski.rsqlpresentation.repo.AnimalRepository;
 import pl.baranowski.rsqlpresentation.service.RSQLService;
 
-import java.util.List;
 import java.util.Optional;
 
 @RequiredArgsConstructor
@@ -20,13 +21,13 @@ public class AnimalController {
     private final AnimalRepository repository;
 
     @GetMapping
-    List<Animal> find(@RequestParam(value = "query", required = false) String query) {
+    Page<Animal> find(@RequestParam(value = "query", required = false) String query, Pageable pageable) {
         Optional<Specification<Animal>> specification = rsqlService.toSpecification(query);
 
         if (specification.isPresent()) {
-            return repository.findAll(specification.get());
+            return repository.findAll(specification.get(), pageable);
         }
 
-        return repository.findAll();
+        return repository.findAll(pageable);
     }
 }
